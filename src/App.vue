3e4f4e84addb9e730d399525deddb36a1eb4b47e
Alan Hiese <alan.hiese@gmail.com>
2025-01-28 21:15:25 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  const hola = "hola mundo"
+</script>
+
+<template>
+  <h1>{{ hola }}</h1>
+</template>
+
+<style scoped>
+
+h1 {
+  text-transform: uppercase;
+  color: red
+}
+
+</style>
